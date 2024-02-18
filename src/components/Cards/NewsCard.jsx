1a95d09bb78325ee83a8b6eab741cac8c4b156d1
Prabhat2912 <pk993105@gmail.com>
2024-02-18 22:52:33 +0530

    res12

--- src/components/Cards/NewsCard.jsx
+++ src/components/Cards/NewsCard.jsx
@@ -5,7 +5,7 @@ const NewsCard = ({ data1 }) => {
   return (
     <Link to={data1.url} target="blank">
       {" "}
-      <div className="w-[285px] h-[300px] bg-white p-1 rounded-lg hover:transform hover:scale-105 transition-transform duration-300 ease-in-out ">
+      <div className="w-[270px] h-[300px] bg-white p-1 rounded-lg hover:transform hover:scale-105 transition-transform duration-300 ease-in-out ">
         <div className="mb-4">
           <img src={data1.thumbnail} alt="thubmnail" className="rounded-lg" />
         </div>
